d376b52811f0c2f04ee100206593f339d35f517c
shinsuke-fujita1126 <shinsuke-fujita@cybozu.co.jp>
2022-11-17 23:06:24 +0900

    initial commit

---
+++ app/src/main/java/com/example/migration_view_to_compose/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.migration_view_to_compose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.migration_view_to_compose.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
