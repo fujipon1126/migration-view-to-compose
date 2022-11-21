596ef41ffe19fa32f4c8fe22673f9977502b1ad0
fujipon1126 <fujitansp@gmail.com>
2022-11-22 01:07:22 +0900

    MainFragmentの1つのボタンをCommonButtonに置き換え

--- app/src/main/java/com/example/migration_view_to_compose/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/migration_view_to_compose/ui/main/MainFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.migration_view_to_compose.MainActivity
 import com.example.migration_view_to_compose.R
+import com.example.migration_view_to_compose.common_ui.ui.CommonButton
 import com.example.migration_view_to_compose.databinding.FragmentMainBinding
+import com.google.android.material.composethemeadapter.MdcTheme
 
 class MainFragment : Fragment() {
 
@@ -39,8 +44,20 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
-            sub1Button.setOnClickListener {
-                mainActivity?.navigateTo(R.id.sub1_fragment)
+            // sub1ButtonをComposeViewに置き換え
+//            sub1Button.setOnClickListener {
+//                mainActivity?.navigateTo(R.id.sub1_fragment)
+//            }
+            sub1Button.setContent {
+                MdcTheme {
+                    // common-uiのボタンを利用する
+                    CommonButton(
+                        modifier = Modifier.padding(start = 8.dp, top = 8.dp),
+                        text = "To Sub1"
+                    ) {
+                        mainActivity?.navigateTo(R.id.sub1_fragment)
+                    }
+                }
             }
             sub2Button.setOnClickListener {
                 mainActivity?.navigateTo(R.id.sub2_fragment)
